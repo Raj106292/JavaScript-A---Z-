0baa0c586e7b200b7ad6ee3adfda2e166ca0db05
Raj106292 <rajdas.106292@gmail.com>
2023-04-12 00:51:59 +0600

    javascript start

---
+++ 01.js
@@ -0,0 +1,17 @@
+/* ---------------------------------------- Introduction ---------------------------------------- */
+/* 
+    => the programs in JavaScript language are called scripts
+    => name of JavaScript -> LiveScript, JavaScript, EcmaScript
+    => JS engine:
+        -> V8 engine: Chrome, Opera, Edge
+        -> Spider Monkey: FireFox
+        -> Squirrel Fish: Safari
+        -> Chakra: IE
+
+    => three great things of JavaScript:
+        -> full integration with HTML & CSS
+        -> simple things done simply
+        -> supported by all major browsers and enabled by default
+
+    => IDE: integrated development environment
+*/
